5a82d1ff1852da7d590513897b8b9b21959809ff
Koki <xoishko@amazon.co.jp>
2026-02-27 19:12:17 +0900

    delete sample2.py

--- sample2.py
+++
@@ -1,5 +0,0 @@
-def change_file_permissions_noncompliant():
-    import os
-    import stat
-    # Noncompliant: permissions assigned to all users.
-    os.chmod("sample.txt", stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
